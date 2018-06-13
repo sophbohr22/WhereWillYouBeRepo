51db5bb170e2fe3577a9c886102c8481d9e8ab1d
Unknown <19parfrg@elmbrookstudents.org>
2018-06-13 10:48:02 -0500

    Added Grants stuff

--- FallLocation/Program.cs
+++ FallLocation/Program.cs
@@ -7,7 +7,8 @@ namespace FallLocation
         static void Main(string[] args)
         {
             Console.WriteLine("Sophie will be at UW-Madison");
-			Console.WriteLine("Kate will till be at Ike, unfortunately.");
+			Console.WriteLine("Kate will still be at Ike, unfortunately.");
+            Console.WriteLine("Grant will be at Brookfield Central.");
 			Console.ReadKey(); 
         }
     }
