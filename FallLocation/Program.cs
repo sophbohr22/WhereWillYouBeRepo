69391047a1c12aa674a3052cdf783eaefacee87d
Unknown <soph.bohr22@gmail.com>
2018-06-13 10:34:50 -0500

    Added sophie's location

--- FallLocation/Program.cs
+++ FallLocation/Program.cs
@@ -6,7 +6,7 @@ namespace FallLocation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sophie will be at UW-Madison");
         }
     }
 }
