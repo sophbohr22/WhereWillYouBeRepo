6d759ca7b6bf52e88aca444dab4c794ce67a1d96
Matthew Chiang <mchiang2112@gmail.com>
2018-06-13 10:54:52 -0500

    Added Where Matthew Will attend next year

--- FallLocation/Program.cs
+++ FallLocation/Program.cs
@@ -9,6 +9,7 @@ namespace FallLocation
             Console.WriteLine("Sophie will be at UW-Madison");
 			Console.WriteLine("Kate will still be at Ike, unfortunately.");
             Console.WriteLine("Grant will be at Brookfield Central.");
+            Console.WriteLine("Mattehw will be attending UW-Madison");
 			Console.ReadKey(); 
         }
     }
